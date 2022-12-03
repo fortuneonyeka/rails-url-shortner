c1d2aadbaec0aeac5ad12c126c2f9b1e4ff82d77
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-12-03 08:35:39 +0100

    created short url with flash messages

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,9 +1,24 @@
 class UrlsController < ApplicationController
   def index
-   
+    @url = Url.new
   end
 
   def create
-    @url = Url.new 
+    url = Url.new permitted_params
+
+    if url.save
+      flash[:success] = "<p>shortened URL successfully created!</p> <a  href='#{url.shortend_url}'target='_blank'>#{url.shortend_url}</a>"
+    else
+      flash[:error] = "<p>Invalid Url #{url.errors.full_messages.join()} that contain http or https</p>"
+    end
+      redirect_to root_path
   end
+
+
+  private
+
+  def permitted_params
+    params.require(:url).permit(:name)
+  end
+  
 end
